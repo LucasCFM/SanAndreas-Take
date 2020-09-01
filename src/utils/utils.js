9b49a576ba487b228ddec136ce4b0ed4c97af32d
Lucas Felicíssimo <lucas.cesar.ti@gmail.com>
2020-09-01 00:01:35 -0300

    Createad utils with absoluteFilePath and fileType

---
+++ src/utils/utils.js
@@ -0,0 +1,28 @@
+import path from 'path';
+
+
+// construct a absolute path based on root-script path
+const getAbsoluteFromRelativePath = ( relativePath ) => {
+    // relativePath must be relative from the script root path
+    // root scritp: main.js
+    const rootPath = process.cwd()
+    
+    if( relativePath.charAt(0) !== '/' )
+        relativePath = '/' + relativePath;
+    
+    return rootPath + relativePath;
+}
+
+
+// returns the last extension of a file
+const getFileType = ( fileName ) => {
+    let fileType = path.extname( fileName ).split( '.' );
+    fileType = fileType[ fileType.length - 1 ];
+    return fileType;
+};
+
+
+export {
+    getAbsoluteFromRelativePath,
+    getFileType
+};
