1ec43248087317f91b2214b1df0631f531a9d327
Lucas Felicíssimo <lucas.cesar.ti@gmail.com>
2020-09-01 05:57:19 -0300

    Better mapping & removed console logs

--- src/handler/packageHandler.js
+++ src/handler/packageHandler.js
@@ -11,18 +11,12 @@ const formatPackage = ( packageLine ) => {
 
 // maps lines on package file to a dictionary of origins and destinies
 const readPackages = async ( filePath ) => {
-    let linesOfPackages = await readFile( filePath );
-    const packages = linesOfPackages.map( function( line ) {
-        const packagePieces = line.split( ' ' );
-        return {
-            origin: packagePieces[0],
-            destiny: packagePieces[1],
-        };
-    });
-    console.log( 'readPackages packages:' );
-    console.log( packages );
-    // return packages;
-    // TODO: return readFile( filePath ).map( formatPackage );
+    const linesOfPackages = await readFile( filePath );
+    const packages = linesOfPackages.map( formatPackage );
+    return packages;
 };
 
-export default readPackages;
+
+export {
+    readPackages
+};
