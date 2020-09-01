cad89fbc7193a87ef725b7d81495477d9a3c2152
Lucas Felicíssimo <lucas.cesar.ti@gmail.com>
2020-09-01 10:02:36 -0300

    Added main flux and data files

--- src/main.js
+++ src/main.js
@@ -1,16 +1,14 @@
-import 'module-alias/register.js';
-import dijkstra from './dijkstra/dijkstra.js';
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+require('module-alias/register');
 
-const graph = {
-    LS: { SF: 1, LV: 1, RC: 1 },
-    SF: { LS: 2, WS: 3, WS: 4, WS: 6, LV: 2 },
-    LV: { LS: 1, SF: 2, BC: 1 },
-    WS: { SF: 2 },
-    BC: { LV: 1 },
-    RC: { LS: 2 },
-};
+import { getAbsoluteFromRelativePath } from './utils/utils.js';
+import { runShortestPaths } from './shortestPath/shortestPath.js'
 
-// TODO: Remove more than one path to same location, leave only the smalllest
-// ASK THEM IF THIS CAN HAPPEN
 
-console.log( dijkstra(graph, 'SF', 'WS') );
+const pathFileRelativePath = '/src/path.txt';
+const pathFileAbsolutePath = getAbsoluteFromRelativePath( pathFileRelativePath );
+const packageFileRelativePath = '/src/package.txt';
+const packageFileAbsolutePath = getAbsoluteFromRelativePath( packageFileRelativePath );
+
+runShortestPaths( pathFileAbsolutePath, packageFileAbsolutePath );
