4fece3dc0f0762ba818e88f67fa3bc58cccfb1ca
Lucas Felicíssimo <lucas.cesar.ti@gmail.com>
2020-09-01 02:43:57 -0300

    code clean up

--- src/handler/fileHandler.js
+++ src/handler/fileHandler.js
@@ -6,14 +6,10 @@ const readFile = async ( filePath ) => {
     // checks if file extension. Must be .txt file
     
     const fileType = getFileType( filePath );
-    console.log( 'fileType:' )
-    console.log( fileType )
     if( fileType !== 'txt' )
         throw Error( 'Only text files allowed' )
     
-    console.log('ANTES DO DATA')
     const data = await fs.promises.readFile(filePath, 'utf8');
-    console.log('PASSOU DO DATA')
     const lines = data.toString().split("\n");
     for( i in lines ) {
         console.log( 'readFile line:' );
